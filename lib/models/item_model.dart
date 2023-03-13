450f0b9d7a24a10497012bef2f52857b308a0b48
Rafal Siudy <siudyysraf@gmail.com>
2023-03-13 13:49:12 -0700

    add itemmodel

---
+++ lib/models/item_model.dart
@@ -0,0 +1,12 @@
+class ItemModel {
+  ItemModel(
+      {required this.title,
+      required this.imgUrl,
+      required this.realeasedata,
+      required this.id});
+
+  final String title;
+  final String imgUrl;
+  final DateTime realeasedata;
+  final String id;
+}
